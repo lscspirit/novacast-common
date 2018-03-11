154bc9c294fd7939677101c4b49524f6246b4cd8
lscspirit <lscspirit@hotmail.com>
2018-03-11 22:54:08 +0800

    Add thread id to log format.

--- lib/novacast-common/logging/formatter.rb
+++ lib/novacast-common/logging/formatter.rb
@@ -3,12 +3,12 @@ module Novacast
     class Formatter < ActiveSupport::Logger::SimpleFormatter
       include ActiveSupport::TaggedLogging::Formatter
 
-      ENTRY_FORMAT    = "%s (#%d)%s %5s -- %s\n"
+      ENTRY_FORMAT    = "%s (#%d - %s)%s %5s -- %s\n"
       DATETIME_FORMAT = '%Y-%m-%dT%T.%L%z'.freeze
 
       def call(severity, time, progname, msg)
         tags_str = (tags = tags_text).blank? ? '' : " #{tags}"
-        ENTRY_FORMAT % [format_datetime(time), $$, tags_str, severity, msg2str(msg)]
+        ENTRY_FORMAT % [format_datetime(time), $$, Thread.current.object_id, tags_str, severity, msg2str(msg)]
       end
 
       private
